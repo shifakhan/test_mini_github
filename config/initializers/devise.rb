3709297b94008200e35f3298b01cdf773056a3f9
shifakhan <findshifa@gmail.com>
2021-04-12 12:02:50 +0530

    Fix devise secrets

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, '5642e9557fc6ddceb5bf', '258975c7ac42dd5c0700a5f158889dffe8c4a582', scope: 'user,public_repo'
+  config.omniauth :github, Rails.application.credentials.config[:devise][:github_id], Rails.application.credentials.config[:devise][:github_secret], scope: 'user,public_repo'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
